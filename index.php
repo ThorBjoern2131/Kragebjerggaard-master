cd07ca5871bb18907f2850b0b6a9bdc0971029ff
madsr <madsrosendal@live.dk>
2022-12-15 12:13:17 +0100

    Fjernelse af produkter fra forsiden

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require "settings/init.php";
 
-$produkter = $db->sql("SELECT * FROM produkter");
+$produkter = $db->sql("SELECT * FROM produkter where prodId= 10");
 
 ?>
 <!-- Instruktion til webbrowser om at vi kører HTML5 -->
